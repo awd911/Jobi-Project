b4ebd02fada043e228142b26b87f2b0163a862a1
awd911 <36082238+awd911@users.noreply.github.com>
2023-05-01 15:50:38 -0400

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,58 @@
 import Head from 'next/head'
 import Header from '@components/Header'
-import Footer from '@components/Footer'
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
+import searchResults from "pages/searchResults";
+import { Outlet } from 'react-router-dom';
+import Link from "next/link"
+//mport styles from '../styles/globals.css'
 
-export default function Home() {
+import {functionName} from '@components/searchScripts.js'
+import {search} from '@components/searchScripts.js'
+import Footer from '@components/Footer'
+import {handleSubmit} from 'pages/searchResults.js'
+import { useRouter } from 'next/router';
+import { useState } from 'react';
+
+export async function getStaticProps() {
+  return {
+    props: {
+      heading: 'Jobi Search Engine',
+      details: 'This response is static.',
+    },
+  };
+}
+
+export default function Home({ heading, details }) {
+
+  const router = useRouter();
+  const [searchText, setsearchText] = useState("");
   return (
-    <div className="container">
+    <div>
       <Head>
-        <title>Next.js Starter!</title>
+        <title id="title">{heading}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
+    <main >
+      <h2 id="heading">
+        {heading}
+      </h2>
 
-      <Footer />
-    </div>
+      <div>
+
+    <form action="/searchResults" method="GET">
+        <input type="text" id="keyword" name="keyword" placeholder="Search..."></input>
+        <button type="submit" id="submit">Search</button>
+    </form>
+    <span className="details">{details}</span>
+      </div>
+    </main>
+  </div>
+        
+    //     <button className="btn btn-outline-secondary" id="searchBtn" type="button" onClick={search} >Search</button>
+    //     {/* <Link to="/searchResults">Search Results</Link> */}
+
   )
+
 }
